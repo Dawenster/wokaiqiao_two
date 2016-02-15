2146256f96f8a43dec404c00ec3cd692e93527be
David Wen <dstwen@gmail.com>
2016-02-14 20:18:08 -0800

    Add mailcatcher settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,8 @@ Rails.application.configure do
 
   # For devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Mail catcher settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
