8aaa9b2b4a40182edfdb2b73f16ffd29309106b9
David Wen <dstwen@gmail.com>
2016-02-15 15:31:45 -0800

    Fixed nav for mobile and normal view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,43 @@ module ApplicationHelper
   def become_expert_email
     "experts@wokaiqiao.com"
   end
+
+  def nav_list
+    [
+      {
+        text: "找专家",
+        link: "#"
+      },
+      {
+        text: "常见问题 + 提示",
+        link: "#"
+      },
+      {
+        text: "合作伙伴",
+        link: "#"
+      }
+    ]
+  end
+
+  def not_logged_in_user_actions_list
+    [
+      {
+        text: "注册",
+        link: "#"
+      },
+      {
+        text: "快速登入",
+        link: "#"
+      }
+    ]
+  end
+
+  def logged_in_user_dropdown_list
+    [
+      {
+        text: "个人主页",
+        link: "#"
+      }
+    ]
+  end
 end
