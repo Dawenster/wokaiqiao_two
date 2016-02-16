bd53cc4be363e869e4ab75c8e88ba71f2f4e6f6b
David Wen <dstwen@gmail.com>
2016-02-15 18:09:08 -0800

    Add name and expert to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215222343) do
+ActiveRecord::Schema.define(version: 20160216020815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20160215222343) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "name"
+    t.boolean  "expert"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
