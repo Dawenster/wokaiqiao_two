83c518b10ea5c0041e423999286813e56596d990
David Wen <dstwen@gmail.com>
2016-03-05 15:39:13 -0800

    Set English in rails admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_english_in_rails_admin
 
   protected
 
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_english_in_rails_admin
+    if self.kind_of? RailsAdmin::ApplicationController
+      I18n.locale = :en
+    else
+      I18n.locale = :zh
+    end
   end
 end
