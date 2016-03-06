e05e6ba33fd6d23f5d556b36ed823a8038b2f15d
David Wen <dstwen@gmail.com>
2016-03-05 20:21:23 -0800

    Add title to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306002038) do
+ActiveRecord::Schema.define(version: 20160306042038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160306002038) do
     t.string   "languages"
     t.string   "location"
     t.boolean  "domestic"
+    t.string   "title"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
