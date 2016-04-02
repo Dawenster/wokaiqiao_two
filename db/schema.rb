5826dc3d76565a547850f13f3aa4e2edf9c8bae2
David Wen <dstwen@gmail.com>
2016-04-02 16:03:31 -0700

    Add stripe cus id to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307073320) do
+ActiveRecord::Schema.define(version: 20160402224200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20160307073320) do
     t.string   "title"
     t.string   "phone"
     t.string   "wechat"
+    t.string   "stripe_cus_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
