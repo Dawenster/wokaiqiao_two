7399275e685dcd70e7846b7609c251350bb7b1e6
David Wen <dstwen@gmail.com>
2016-04-16 11:50:52 -0700

    Presence validation for education

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,6 +1,10 @@
 class Education < ActiveRecord::Base
   belongs_to :user
 
+  validates :name,
+            :user_id,
+            presence: true
+
   def name_with_year
     "#{year}年#{name}"
   end
