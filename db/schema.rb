8ff7d351d90440750b2cdec087c14fab6105856a
David Wen <dstwen@gmail.com>
2016-04-16 10:59:11 -0700

    Add currency to charges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416175405) do
+ActiveRecord::Schema.define(version: 20160416175828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160416175405) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "charge_type"
+    t.string   "currency"
   end
 
   create_table "educations", force: :cascade do |t|
