fee3c193e5939cfa8ccd090d1ac381adc028883f
David Wen <dstwen@gmail.com>
2016-04-16 11:50:16 -0700

    Presence validation for charges columns

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,6 +2,13 @@ class Charge < ActiveRecord::Base
   belongs_to :call
   belongs_to :user
 
+  validates :amount_in_cents,
+            :user_id,
+            :stripe_py_id,
+            :charge_type,
+            :currency,
+            presence: true
+
   PAYMENT = "payment"
   REFUND = "refund"
 end
