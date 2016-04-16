54784879e4d25fc262188f6e524aef08af1e159c
David Wen <dstwen@gmail.com>
2016-04-16 11:56:40 -0700

    Charge validation - cannot refund more than total charged

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -8,7 +8,16 @@ class Charge < ActiveRecord::Base
             :charge_type,
             :currency,
             presence: true
+  validate :cannot_refund_more_than_charged
 
   PAYMENT = "payment"
   REFUND = "refund"
+
+  private
+
+  def cannot_refund_more_than_charged
+    if call.present? && call.charges.sum(:amount_in_cents) < 0
+      errors.add(:base, "Cannot refund more than total charged")
+    end
+  end
 end
