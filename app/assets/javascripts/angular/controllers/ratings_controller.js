f8a46c58d3448bebac6a7f9e840ce6be39800198
David Wen <dstwen@gmail.com>
2016-04-17 16:03:49 -0700

    Prevent no-star rating in front-end

--- app/assets/javascripts/angular/controllers/ratings_controller.js
+++ app/assets/javascripts/angular/controllers/ratings_controller.js
@@ -10,12 +10,26 @@ app.controller('RatingsCtrl', ['$scope', '$element', function($scope, $element) 
 
   $(".blank-user-raty-div").raty({
     scoreName: "call[user_rating]",
-    score: $element.data("user-rating")
+    score: $element.find(".blank-user-raty-div").data("user-rating")
   });
 
   $(".blank-expert-raty-div").raty({
     scoreName: "call[expert_rating]",
-    score: $element.data("expert-rating")
+    score: $element.find(".blank-expert-raty-div").data("expert-rating")
   });
+
+  $("body").on("click", ".leave-rating-button", function(e) {
+    e.preventDefault()
+    $(".alert-text").addClass("hide")
+
+    var userRating = $element.find(".blank-user-raty-div input").val()
+    var expertRating = $element.find(".blank-expert-raty-div input").val()
+
+    if (!userRating && !expertRating) {
+      $(".alert-text").removeClass("hide")
+    } else {
+      $element.find("form").submit()
+    }
+  })
 
 }]);
