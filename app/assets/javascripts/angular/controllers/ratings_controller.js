7c51465e3f7613e10506c5fa57dbdf6a93784e6c
David Wen <dstwen@gmail.com>
2016-04-21 22:29:43 -0700

    Fix up little errors around review list

--- app/assets/javascripts/angular/controllers/ratings_controller.js
+++ app/assets/javascripts/angular/controllers/ratings_controller.js
@@ -4,7 +4,7 @@ app.controller('RatingsCtrl', ['$scope', '$element', function($scope, $element) 
 
   if ($element.hasClass("readonly")) {
     var score = $element.data("rating")
-    $(".raty-div").raty({
+    $element.raty({
       score: score,
       readOnly: true,
       noRatedMsg: "还没有评分"
@@ -12,26 +12,26 @@ app.controller('RatingsCtrl', ['$scope', '$element', function($scope, $element) 
   }
 
   if ($element.hasClass("ratable")) {
-    $(".blank-user-raty-div").raty({
+    $element.find(".blank-user-raty-div").raty({
       scoreName: "call[user_rating]",
       score: $element.find(".blank-user-raty-div").data("user-rating")
     });
 
-    $(".blank-expert-raty-div").raty({
+    $element.find(".blank-expert-raty-div").raty({
       scoreName: "call[expert_rating]",
       score: $element.find(".blank-expert-raty-div").data("expert-rating")
     });
   }
 
-  $("body").on("click", ".leave-rating-button", function(e) {
+  $element.on("click", ".leave-rating-button", function(e) {
     e.preventDefault()
-    $(".alert-text").addClass("hide")
+    $element.find(".alert-text").addClass("hide")
 
     var userRating = $element.find(".blank-user-raty-div input").val()
     var expertRating = $element.find(".blank-expert-raty-div input").val()
 
     if (!userRating && !expertRating) {
-      $(".alert-text").removeClass("hide")
+      $element.find(".alert-text").removeClass("hide")
     } else {
       $element.find("form").submit()
     }
