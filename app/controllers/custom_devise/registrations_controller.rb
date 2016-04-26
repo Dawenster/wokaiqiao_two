0a14812ed0bf61ed41c7f3ecd083a23b36604d93
David Wen <dstwen@gmail.com>
2016-04-25 21:36:04 -0700

    Can edit name, phone, and wechat

--- app/controllers/custom_devise/registrations_controller.rb
+++ app/controllers/custom_devise/registrations_controller.rb
@@ -5,4 +5,24 @@ class CustomDevise::RegistrationsController < Devise::RegistrationsController
     Emails::User.send_welcome(resource)
   end
 
+  def update
+    if params[:user][:password].present? && !resource.valid_password?(params[:user][:current_password])
+      flash[:alert] = "你的密码不正确"
+      return redirect_to edit_user_registration_path
+    else
+      super
+    end
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    params.delete(:current_password)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
+
 end 
