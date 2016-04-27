6746f1ea6e08e2a7b1a663f242e51264141ccd5e
David Wen <dstwen@gmail.com>
2016-04-26 22:00:41 -0700

    Add user_id to credits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426055336) do
+ActiveRecord::Schema.define(version: 20160427050007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160426055336) do
     t.integer  "call_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "educations", force: :cascade do |t|
