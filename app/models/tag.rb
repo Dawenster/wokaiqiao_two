2786e3f5a49f1a75b8b08cbbd661d1b88a612e09
David Wen <dstwen@gmail.com>
2016-05-03 23:36:09 -0700

    Model relations between tags and users (experts)

---
+++ app/models/tag.rb
@@ -0,0 +1,5 @@
+class Tag < ActiveRecord::Base
+  has_and_belongs_to_many :experts, class_name: User, join_table: "experts_tags", association_foreign_key: "expert_id"
+
+  validates :name, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ActiveRecord::Base
   has_many :refunds
   has_many :payouts, class_name: Payout, foreign_key: :expert_id
   has_and_belongs_to_many :promotions, uniq: true
+  has_and_belongs_to_many :tags, class_name: Tag, join_table: "experts_tags", foreign_key: "tag_id"
 
   def s3_credentials
     {
