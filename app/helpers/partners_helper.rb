c43dad4777a51f84411cd62b41ac7ffe6d263c25
David Wen <dstwen@gmail.com>
2016-05-26 00:33:12 -0700

    Setup framework for partners page

---
+++ app/helpers/partners_helper.rb
@@ -0,0 +1,24 @@
+module PartnersHelper
+  def school_partners
+    [
+      {
+        name: "北京大學",
+        logo: "",
+        activity: "",
+        activity_occurred_at: Time.current
+      },
+      {
+        name: "中山大學",
+        logo: "",
+        activity: "",
+        activity_occurred_at: Time.current
+      },
+      {
+        name: "交通大學",
+        logo: "",
+        activity: "",
+        activity_occurred_at: Time.current
+      }
+    ]
+  end
+end
