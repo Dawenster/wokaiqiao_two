725d79546656b825db639ef1f9d54557acd12d70
David Wen <dstwen@gmail.com>
2016-08-06 16:47:02 -0700

    Invite guest via Cloopen

--- app/models/cloopen/conference.rb
+++ app/models/cloopen/conference.rb
@@ -14,17 +14,40 @@ module Cloopen
       payload = <<-eos
         <?xml version='1.0' encoding='utf-8'?>
         <Request>
-          <Appid>8a216da855ce635b0155cf988a540083</Appid>
+          <Appid>#{ENV["CLOOPEN_APP_ID"]}</Appid>
           <CreateConf action='createconfresult.jsp' maxmember='#{maxmember}'/>
         </Request>
       eos
       # Clean up newline and double spaces
-      payload.gsub!("\n", "").gsub!("  ", "")
-      Hash.from_xml(RestClient.post url("createconf"), payload, @header)["Response"]
+      payload = clean_up_payload(payload)
+      full_url = url("createconf", {maxmember: maxmember})
+      Hash.from_xml(RestClient.post full_url, payload, @header)["Response"]
     end
 
-    def url(action)
-      @url = "#{Cloopen::Controller.base_uri}/ivr/#{action}?sig=#{@sig_parameter}"
+    def invite_guest(conf_id, number)
+      payload = <<-eos
+        <?xml version='1.0' encoding='utf-8'?>
+        <Request>
+          <Appid>#{ENV["CLOOPEN_APP_ID"]}</Appid>
+          <InviteJoinConf confid='#{conf_id}' number='#{number}' />
+        </Request>
+      eos
+      # Clean up newline and double spaces
+      payload = clean_up_payload(payload)
+      full_url = url("conf", {confid: conf_id})
+      Hash.from_xml(RestClient.post full_url, payload, @header)["Response"]
+    end
+
+    def url(action, options)
+      action_url = "#{Cloopen::Controller.base_uri}/ivr/#{action}?sig=#{@sig_parameter}"
+      options.each do |key, value|
+        action_url += "&#{key}=#{value}"
+      end
+      action_url
+    end
+
+    def clean_up_payload(payload)
+      payload.gsub("\n", "").gsub("  ", "")
     end
 
   end
